611dd416276eb46134562f5cb1486182c7a08e0a
Joseph L <jxrlee@gmail.com>
2014-06-10 20:32:33 -0700

    Initial Commit

---
+++ facebook-navigationTests/facebook_navigationTests.m
@@ -0,0 +1,34 @@
+//
+//  facebook_navigationTests.m
+//  facebook-navigationTests
+//
+//  Created by Joseph Lee on 6/10/14.
+//  Copyright (c) 2014 mn8. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface facebook_navigationTests : XCTestCase
+
+@end
+
+@implementation facebook_navigationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
